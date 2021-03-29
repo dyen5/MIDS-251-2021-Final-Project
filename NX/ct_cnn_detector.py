bebc7fa49e69f043224b75a9a5a733efc924fd8b
papale47 <46306242+papale47@users.noreply.github.com>
2021-03-28 22:09:24 -0500

    Update ct_cnn_detector.py

--- NX/ct_cnn_detector.py
+++ NX/ct_cnn_detector.py
@@ -115,28 +115,6 @@ def predict_CNN(model, image):
     print(prediction_label, '; Probability of Diagnosis:', np.exp(probs[0][prediction][0].cpu().item()) )
 
 
-def center_crop(img, dim):
-    """Returns center cropped image
-	
-    Args:Image Scaling
-    img: image to be center cropped
-    dim: dimensions (width, height) to be cropped from center
-    """
-
-    width, height = img.shape[1], img.shape[0]
-	
-    #process crop width and height for max available dimension
-    crop_width = dim[0] if dim[0]<img.shape[1] else img.shape[1]
-    crop_height = dim[1] if dim[1]<img.shape[0] else img.shape[0] 
-	
-    mid_x, mid_y = int(width/2), int(height/2)
-    cw2, ch2 = int(crop_width/2), int(crop_height/2) 
-    crop_img = img[mid_y-ch2:mid_y+ch2, mid_x-cw2:mid_x+cw2]
-        
-    return crop_img
-
-
-
 #Model Initializiation
 CNN_Model = CNN(filter_sizes = [2, 3, 4], 
                 num_filters = [25,25,25],
